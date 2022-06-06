27a7157a956b5d95f07d8b2b29d61c5fcd8c4f4d
awxkee <qnljoy@gmail.com>
2022-06-06 22:47:23 +0200

    built for Mac Catalyst

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "libyuv",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v11),
+        .macCatalyst(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
